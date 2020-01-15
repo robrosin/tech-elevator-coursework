662889aa8622859083bdeafbe52bed5f11366941
Mike Morel <michael.morel@techelevator.com>
2020-01-15 13:24:15 -0500

    M1D3 lecture

---
+++ module-1/03_Expressions/lecture-final/Lecture/16_ReturnTrueWhenLargeEvenNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    public partial class LectureExample
+    {
+        /*
+        16. Return "Big Even Number" when number is even, larger than 100, and a multiple of 5.
+            Return "Big Number" if the number is just larger than 100.
+            Return empty string for everything else.
+            TOPIC: Complex Expression
+        */
+        public string ReturnBigEvenNumber(int number)
+        {
+            if (number > 100)
+            {
+                if (number % 2 == 0 && number % 5 == 0 )
+                {
+                    return "Big Even Number";
+                }
+                else
+                {
+                    return "Big Number";
+                }
+            }
+            return "";
+        }
+        
+
+
+    }
+}
---
+++ module-1/03_Expressions/lecture-final/Lecture/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*****************************************************************************
+            Part 1: Variable Scope
+            ******************************************************************************/
+            // Declare a variable
+            int outerVariable = 5;
+            Console.WriteLine("outerVariable is " + outerVariable);
+
+            // Start a statement block
+            {
+
+                // Print out the variable defined in the outer scope
+                Console.WriteLine("outerVariable is " + outerVariable);
+
+                // Declare a variable in the block (inner scope)
+                int innerVariable = 10;
+
+                // Print out that variable
+                Console.WriteLine("Inner variable is " + innerVariable);
+
+                // End the statement block
+            }
+            // Print the the variable declared in the block
+            // OOOPS, cannot do this!
+            //Console.WriteLine("Inner variable is " + innerVariable);
+
+
+            /*****************************************************************************
+            Part 2: Methods
+            ******************************************************************************/
+            // Call the MultiplyBy method
+            int height = 10;
+            int width = 20;
+            int area = MultiplyBy(height, width);
+            Console.WriteLine($"The area of a rectangle {height} by {width} is {area}");
+
+            // Call MultiplyBy a few more different ways
+            int a = 0;
+            int answer = MultiplyBy(a, 300);
+
+            answer = MultiplyBy(10 * 30, 5 * 15);
+
+            a = -250;
+            answer = MultiplyBy(Math.Abs(a), 10);
+
+            // Create and print some boolean expressions
+
+
+            Console.ReadKey();
+        }
+
+        /*
+         * Create a method called MultiplyBy, which takes two integers and returns the integert product.
+         */
+        static public int MultiplyBy(int multiplicand, int multiplier)
+        {
+            int product = multiplicand * multiplier;
+            return product;
+        }
+
+    }
+}
