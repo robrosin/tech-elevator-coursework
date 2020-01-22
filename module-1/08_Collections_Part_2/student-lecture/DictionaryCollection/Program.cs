db6ff6cd6e36338c4df72a7cd81a0548b0b44c58
Mike Morel <michael.morel@techelevator.com>
2020-01-22 08:39:03 -0500

    M1D8 student

---
+++ module-1/08_Collections_Part_2/student-lecture/DictionaryCollection/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DictionaryDemo();
+            
+            //HashSetDemo();
+
+            // Build a name / height database and search it
+            RunHeightDatabase();
+            //Console.ReadLine();
+        }
+
+        static void DictionaryDemo()
+        {
+            // Demonstrate creating and searching a dictionary
+        }
+
+        static void HashSetDemo()
+        {
+            // Demonstrate a few HashSet methods
+
+        }
+        static void RunHeightDatabase()
+        {
+            // Display a greeting and menu
+
+            //// 1. Let's create a new Dictionary that could hold string, ints
+            ////      | "Josh"    | 70 |
+            ////      | "Bob"     | 72 |
+            ////      | "John"    | 75 |
+            ////      | "Jack"    | 73 |
+            Dictionary<string, int> heightDB = null;
+
+            string input;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(@"     _       _        _                    ");
+                Console.WriteLine(@"    | |     | |      | |                   ");
+                Console.WriteLine(@"  __| | __ _| |_ __ _| |__   __ _ ___  ___ ");
+                Console.WriteLine(@" / _` |/ _` | __/ _` | '_ \ / _` / __|/ _ \");
+                Console.WriteLine(@"| (_| | (_| | || (_| | |_) | (_| \__ \  __/");
+                Console.WriteLine(@" \__,_|\__,_|\__\__,_|_.__/ \__,_|___/\___|");
+                Console.WriteLine(@"                                           ");
+                Console.WriteLine("1) Add / update data");
+                Console.WriteLine("2) Search the database");
+                Console.WriteLine("3) Print the database");
+                Console.WriteLine("4) Get Average Height");
+                Console.WriteLine("Q) Quit");
+                Console.WriteLine("");
+                Console.Write("Please enter selection: ");
+                input = Console.ReadLine().ToLower();
+                if (input.Length > 0)
+                {
+                    input = input.Substring(0, 1);
+                }
+                if (input == "q")
+                {
+                    break;
+                }
+                else if (input == "1")
+                {
+                    AddEditDB(heightDB);
+                }
+                else if (input == "2")
+                {
+                    SearchDB(heightDB);
+                }
+                else if (input == "3")
+                {
+                    PrintDB(heightDB);
+                }
+                else if (input == "4")
+                {
+                    ShowAverageHeight(heightDB);
+                }
+                else
+                {
+                    continue;
+                }
+                Console.WriteLine("Press enter to continue");
+                Console.ReadLine();
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Done...");
+
+
+        }
+
+        public static void ShowAverageHeight(Dictionary<string, int> heightDB)
+        {
+            //7. Let's get the average height of the people in the dictionary
+        }
+
+        public static void PrintDB(Dictionary<string, int> heightDB)
+        {
+            // Looping through a dictionary involves using a foreach loop
+            // to look at each item, which is a key-value pair
+            Console.WriteLine("Printing...");
+
+        }
+
+        public static void AddEditDB(Dictionary<string, int> db)
+        {
+            Console.Write("What is the person's name?: ");
+            string name = Console.ReadLine();
+
+            Console.Write("What is the person's height (in inches)?: ");
+            int height = int.Parse(Console.ReadLine());
+
+            // 2. Check to see if that name is in the dictionary
+            //      bool exists = dictionaryVariable.ContainsKey(key)
+            bool exists = false;    // <-- change this
+
+            if (!exists)
+            {
+                Console.WriteLine($"Adding {name} with new value.");
+                // 3. Put the name and height into the dictionary
+                //      dictionaryVariable[key] = value;
+                //      OR dictionaryVariable.Add(key, value);
+
+            }
+            else
+            {
+                Console.WriteLine($"Overwriting {name} with new value.");
+                // 4. Overwrite the current key with a new value
+                //      dictionaryVariable[key] = value;
+            }
+        }
+        public static void SearchDB(Dictionary<string, int> db)
+        {
+            Console.Write("Which name are you looking for? ");
+            string input = Console.ReadLine();
+
+            //5. Let's get a specific name from the dictionary
+
+
+
+        }
+    }
+}
