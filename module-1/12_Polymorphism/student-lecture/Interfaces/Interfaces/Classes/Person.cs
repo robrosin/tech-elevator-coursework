568d5a4fe6f24ace1305932b5af3b3442d4efc72
Mike Morel <michael.morel@techelevator.com>
2020-01-28 08:44:25 -0500

    M1D12 student

---
+++ module-1/12_Polymorphism/student-lecture/Interfaces/Interfaces/Classes/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces.Classes
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int HeightInches { get; set; }
+
+        public Person(string first, string last, int age, int height)
+        {
+            FirstName = first;
+            LastName = last;
+            Age = age;
+            HeightInches = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: Age {Age}, Height {HeightInches} inches.";
+        }
+
+        //public int CompareTo(object obj)
+        //{
+        //    Person other = (Person)obj;
+
+        //    //// Sort by Last Name
+        //    //return this.LastName.CompareTo(other.LastName);
+
+        //    //// Sort by First Name
+        //    //return this.FirstName.CompareTo(other.FirstName);
+
+        //    //// Sort by Age
+        //    //return this.Age - other.Age;
+
+        //    ////Sort by Height
+        //    //return this.HeightInches - other.HeightInches;
+        //}
+    }
+}
