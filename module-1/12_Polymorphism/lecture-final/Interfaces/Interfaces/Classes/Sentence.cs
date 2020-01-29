27471ced9959f694f677ceb3d95cce2c97a81830
Mike Morel <michael.morel@techelevator.com>
2020-01-28 20:35:23 -0500

    M1D12 lecture

---
+++ module-1/12_Polymorphism/lecture-final/Interfaces/Interfaces/Classes/Sentence.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Interfaces.Classes
+{
+    public class Sentence : IEnumerable
+    {
+        private IEnumerator enumerator;
+
+        public Sentence(string sentence)
+        {
+            enumerator = new SentenceEnumerator(sentence);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            this.enumerator.Reset();
+            return this.enumerator;
+        }
+    }
+
+
+
+    public class SentenceEnumerator : IEnumerator
+    {
+        private string sentence;
+        private string[] words;
+        int index;
+        public SentenceEnumerator(string sentence)
+        {
+            this.sentence = sentence;
+            this.words = sentence.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            this.Reset();
+        }
+
+        public object Current
+        {
+            get
+            {
+                if (index < words.Length)
+                {
+                    return this.words[this.index];
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (index < words.Length-1)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            this.index = -1;
+        }
+    }
+}
---
+++ module-1/12_Polymorphism/lecture-final/Interfaces/Interfaces/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Interfaces.Classes;
+namespace Interfaces
+{
+    class Program
+    {
+        //static char[] digitChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        // This sample program implements the IEnumerable / IEnumerator interfaces in creative ways
+        static void Main(string[] args)
+        {
+            // IComparable
+            SortPeople();
+
+
+            //Sentence sentence = new Sentence("");
+            //foreach (string word in sentence)
+            //{
+            //    Console.WriteLine($"Word: {word}");
+            //}
+            Sentence sentence = new Sentence("The quick brown fox jumped over the lazy dog.");
+            foreach (string word in sentence)
+            {
+                Console.WriteLine($"Word: {word}");
+            }
+
+            Fibonacci fib = new Fibonacci(1000);
+            foreach (int n in fib)
+            {
+                Console.WriteLine(n);
+            }
+
+            Digits digits = new Digits(98734567, 10);
+            foreach (char digit in digits)
+            {
+                Console.Write(digit);
+            }
+            Console.WriteLine();
+            Console.WriteLine("====================================");
+
+            digits = new Digits(1010, 16);
+            foreach (char digit in digits)
+            {
+                Console.Write(digit);
+            }
+            Console.WriteLine();
+            Console.WriteLine("====================================");
+
+            digits = new Digits(99, 2);
+            foreach (char digit in digits)
+            {
+                Console.Write(digit);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static private void SortPeople()
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person("Mike", "Morel", 57, 71),
+                new Person("Peggy", "Collins", 50, 62),
+                new Person("Doug", "Clarke", 58, 74),
+                new Person("Andrew", "Quandt", 25, 69)
+            };
+            DisplayList(people);
+
+            // Sort
+            people.Sort();
+            DisplayList(people);
+
+            Console.ReadLine();
+        }
+        static void DisplayList(List<Person> people)
+        {
+            Console.WriteLine("People:");
+            foreach(Person person in people)
+            {
+                Console.WriteLine($"\t{person}");
+            }
+        }
+    }
+}
