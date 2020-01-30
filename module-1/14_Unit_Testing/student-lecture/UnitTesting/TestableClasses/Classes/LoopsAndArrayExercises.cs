4320dfd18a785754ce869c8df861c4ea903f5d4f
Mike Morel <michael.morel@techelevator.com>
2020-01-30 08:12:45 -0500

    M1D14 student added one more exercise

--- module-1/14_Unit_Testing/student-lecture/UnitTesting/TestableClasses/Classes/LoopsAndArrayExercises.cs
+++ module-1/14_Unit_Testing/student-lecture/UnitTesting/TestableClasses/Classes/LoopsAndArrayExercises.cs
@@ -38,5 +38,26 @@ namespace TestableClasses.Classes
 
             return nums;
         }
+
+        /*
+            Return the sum of the numbers in the array, returning 0 for an empty array. Except the number
+            13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do
+            not count.
+            Sum13([1, 2, 2, 1]) → 6
+            Sum13([1, 1]) → 2
+            Sum13([1, 2, 2, 1, 13]) → 6
+        */
+        public int Sum13(int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 13)
+                {
+                    sum += nums[i];
+                }
+            }
+            return sum;
+        }
     }
 }
