73e872f6978e087a1b40990f128f2ee5df4f1e05
Rob Rosin <robgrosin@gmail.com>
2020-02-04 16:10:59 -0500

    M1D6 FizzBuzzTest Part 2 Complete

---
+++ module-1/16_Test_Driven_Development/student-exercise/Exercises.Tests/KataFizzBuzzTestsPart2.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Tests
+{
+    [TestClass]
+    public class KataFizzBuzzTestsPart2
+    {
+        [TestMethod]
+        public void Three_FizzBuzzTest()
+        {
+            // Arrange
+            KataFizzBuzz sum = new KataFizzBuzz();
+
+            // Act
+            string result = sum.fizzBuzz(3);
+
+            // Assert
+            Assert.AreEqual("Fizz", result);
+        }
+        [TestMethod]
+        public void Thirteen_FizzBuzzTest()
+        {
+            // Arrange
+            KataFizzBuzz sum = new KataFizzBuzz();
+
+            // Act
+            string result = sum.fizzBuzz(13);
+
+            // Assert
+            Assert.AreEqual("Fizz", result);
+        }
+        [TestMethod]
+        public void ThirtyFive_FizzBuzzTest()
+        {
+            // Arrange
+            KataFizzBuzz sum = new KataFizzBuzz();
+
+            // Act
+            string result = sum.fizzBuzz(35);
+
+            // Assert
+            Assert.AreEqual("FizzBuzz", result);
+        }
+        [TestMethod]
+        public void Five_FizzBuzzTest()
+        {
+            // Arrange
+            KataFizzBuzz sum = new KataFizzBuzz();
+
+            // Act
+            string result = sum.fizzBuzz(5);
+
+            // Assert
+            Assert.AreEqual("Buzz", result);
+        }
+        [TestMethod]
+        public void FiftyOne_FizzBuzzTest()
+        {
+            // Arrange
+            KataFizzBuzz sum = new KataFizzBuzz();
+
+            // Act
+            string result = sum.fizzBuzz(51);
+
+            // Assert
+            Assert.AreEqual("Buzz", result);
+        }
+        [TestMethod]
+        public void FiftyThree_FizzBuzzTest()
+        {
+            // Arrange
+            KataFizzBuzz sum = new KataFizzBuzz();
+
+            // Act
+            string result = sum.fizzBuzz(53);
+
+            // Assert
+            Assert.AreEqual("FizzBuzz", result);
+        }
+    }
+}
