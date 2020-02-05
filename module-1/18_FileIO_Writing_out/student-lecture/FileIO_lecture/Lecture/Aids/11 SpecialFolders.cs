282b6feb7b4833aa2460a5dd4c5b2b03abc06992
Mike Morel <michael.morel@techelevator.com>
2020-02-05 08:27:55 -0500

    M1D18 student

---
+++ module-1/18_FileIO_Writing_out/student-lecture/FileIO_lecture/Lecture/Aids/11 SpecialFolders.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Aids
+{
+    public static class SpecialFolders
+    {
+        static public string GetUserFolder()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        }
+
+        static public string GetDesktopFolder()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+        static public string GetMyDocumentsFolder()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+        static public string GetMusicFolder()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+        }
+        static public string GetSharedMusicFolder()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.CommonMusic);
+        }
+
+        static public void ShowAllSpecialFolders()
+        {
+            foreach (int val in Enum.GetValues(typeof(Environment.SpecialFolder)))
+            {
+                string folder = Environment.GetFolderPath((Environment.SpecialFolder)val);
+                Console.WriteLine($"{Enum.GetName(typeof(Environment.SpecialFolder), val)}: {folder}");
+            }
+
+        }
+    }
+}
