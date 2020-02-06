3916e52a0c8a36130e832d3d0d2c76e075c11aa2
Mike Morel <michael.morel@techelevator.com>
2020-02-05 20:30:41 -0500

    M1D18 lecture

---
+++ module-1/18_FileIO_Writing_out/lecture-final/FileIO_lecture/Exceptions/Classes/AccountOverdrawException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions.Classes
+{
+    public class AccountOverdrawException : Exception
+    {
+        public string AccountNumber { get; set; }
+        public decimal OverdrawAmount { get; set; }
+
+        public AccountOverdrawException(string account, decimal overdrawAmount) : base( $"Account overdraw, account {account} attempted to withdraw {overdrawAmount:C} too much!")
+        {
+            this.AccountNumber = account;
+            this.OverdrawAmount = overdrawAmount;
+        }
+    }
+}
---
+++ module-1/18_FileIO_Writing_out/lecture-final/FileIO_lecture/Exceptions/Classes/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions.Classes
+{
+    public class BankAccount
+    {
+        public string AccountNumber { get;}
+        public decimal Balance { get; private set; }
+
+        public BankAccount(decimal intialBalance)
+        {
+            this.AccountNumber = Guid.NewGuid().ToString();
+            this.Balance = intialBalance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            this.Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > this.Balance)
+            {
+//                throw new Exception($"You tried to withdraw {amount:C}, but you only have {Balance:C}!!");
+                throw new AccountOverdrawException(AccountNumber, this.Balance - amount);
+            }
+            this.Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Account {AccountNumber}: Balance = {Balance:C}";
+        }
+    }
+}
