691ed91bdf889c7dda4e909f12a4ea542a13909d
Mike Morel <michael.morel@techelevator.com>
2020-02-17 08:40:30 -0500

    M2D6 student

---
+++ module-2/06_Database_Connectivity_DAO/student-lecture/WorldDB/WorldDB/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading;
+using WorldDB.DAL;
+using WorldDB.Views;
+
+namespace WorldDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code reads the connection string from appsettings.json
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                        IConfigurationRoot configuration = builder.Build();
+            string connectionString = configuration.GetConnectionString("World");
+
+            // TODO 00: Create a Model for Country to go with the City model we already have
+
+            // TODO 01: Create a CountrySqlDAO class (GetCountries, GetCountries(continent), GetCountry(code))
+            // TODO 01a: Create an ICountryDAO interface
+
+
+            // TODO 07: Create a CitySqlDAO class (GetCitiesByCountryCode)
+            // TODO 07a: Create an ICityDAO interface
+
+
+            // TODO 02b: Create a WorldDBMenu, passing in the country dao, and Run it
+            WorldDBMenu menu = new WorldDBMenu();
+            menu.Run();
+
+            // Say goodbye to the user...
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("Goodbye...");
+            Thread.Sleep(1500);
+        }
+    }
+}
