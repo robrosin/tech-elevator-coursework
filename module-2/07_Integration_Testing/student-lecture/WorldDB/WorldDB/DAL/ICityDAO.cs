a660a6e829b432f2d80cd663f81d221bffe2321c
Mike Morel <michael.morel@techelevator.com>
2020-02-18 08:34:27 -0500

    M2D7 student

---
+++ module-2/07_Integration_Testing/student-lecture/WorldDB/WorldDB/DAL/ICityDAO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorldDB.Models;
+
+namespace WorldDB.DAL
+{
+    public interface ICityDAO
+    {
+        /// <summary>
+        /// Gets all cities provided a country code.
+        /// </summary>
+        /// <param name="countryCode">The country code to search for.</param>
+        /// <returns></returns>
+        IList<City> GetCitiesByCountryCode(string countryCode);
+
+        /// <summary>
+        /// Adds a new city.
+        /// </summary>
+        /// <param name="city">The city to add.</param>
+        bool AddCity(City city);
+
+        /// <summary>
+        /// Given an id, get the city identified by that id.
+        /// </summary>
+        /// <param name="id">Id of a city</param>
+        /// <returns>City object for the id, NULL if the id does not exist</returns>
+        City GetCityById(int id);
+
+    }
+}
