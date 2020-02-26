66eaa65c10983874ca718a5a88e2b515790584a5
Mike Morel <michael.morel@techelevator.com>
2020-02-26 13:09:38 -0500

    M3D3 lecture

---
+++ module-3/03-Introduction-to-MVC-Views/lecture-final/ViewsDay1/ViewsDay1/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsDay1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Loop(int number)
+        {
+            ViewData["Num"] = number;
+            return View();
+        }
+    }
+}
