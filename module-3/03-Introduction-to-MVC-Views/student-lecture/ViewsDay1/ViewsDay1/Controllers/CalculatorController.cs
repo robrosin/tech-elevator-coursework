7f6d5fe1866376d0a831cd95fb13ac0c271454a9
Rob Rosin <robgrosin@gmail.com>
2020-02-26 15:19:48 -0500

    CSS Layouts Exercise Complete

---
+++ module-3/03-Introduction-to-MVC-Views/student-lecture/ViewsDay1/ViewsDay1/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsDay1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Loop(int number)
+        {
+            // Home/Calculator/Loop?number=50 QUERY STRING
+
+            ViewData["Num"] = number;
+
+            return View();
+        }
+    }
+}
