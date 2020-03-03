dd5c041bbcd6823d1da2cb24425cb1dab9113987
Mike Morel <michael.morel@techelevator.com>
2020-03-03 08:26:23 -0500

    M3D7 student

---
+++ module-3/07-Forms-and-Controllers-HTTP-POST/student-lecture/Forms.Web/Models/CitySearchVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms.Web.Models
+{
+    public class CitySearchVM
+    {
+        // TODO 02: Add a Display Data Annotation to set a label for these fields
+        public string CountryCode { get; set; }
+        public string District { get; set; }
+        public IList<City> Cities { get; set; }
+
+        // TODO 01a: Add a place to store a list of countries to the view-model
+        //public SelectList CountryList { get; set; }
+    }
+}
