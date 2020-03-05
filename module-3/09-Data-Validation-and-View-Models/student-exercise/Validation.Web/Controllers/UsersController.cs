d2774517db6d679018b34291a60e53f6fbfc47a1
Mike Morel <michael.morel@techelevator.com>
2020-03-05 08:43:11 -0500

    M3D9 student

---
+++ module-3/09-Data-Validation-and-View-Models/student-exercise/Validation.Web/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Validation.Web.Models;
+
+
+namespace Validation.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: User
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        // GET: User/Register
+        // Return the empty registration view
+
+        // POST: User/Register
+        // Validate the model and redirect to confirmation (if successful) or return the
+        // registration view (if validation fails)
+
+        // GET: User/Login
+        // Return the empty login view
+
+        // POST: User/login
+	    // Validate the model and redirect to a confirmation page if validation is
+	    // successful. Return the login view (if validation fails).
+
+        // GET: User/Confirmation
+        // Return the confirmation view
+
+    }
+}
---
+++ module-3/09-Data-Validation-and-View-Models/student-lecture/World/Forms.Web/Models/City.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms.Web.Models
+{
+ /*** TODO 04: Add model validations
+ * Name: Required, string length max 64
+ * CountryCode: Display Name = Country Code, Required, String Length = 3
+ * District: Required
+ * Population: Required, Range 1-99,999,999
+ ***/
+
+    public class City
+    {
+        public int CityId { get; set; }
+
+        public string Name { get; set; }
+        
+        public string CountryCode { get; set; }
+        
+        public string CountryName { get; set; }
+
+        public string District { get; set; }
+
+        public int Population { get; set; }
+
+        public City() { }
+
+        public City(int cityId, String name, string countryCode, string countryName, string district, int population)
+        {
+            CityId = cityId;
+            Name = name;
+            CountryCode = countryCode;
+            CountryName = countryName;
+            District = district;
+            Population = population;
+        }
+    }
+}
