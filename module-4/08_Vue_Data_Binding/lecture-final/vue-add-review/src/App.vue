fa8f5dd254c680c9c40e32e545d9925547fa1092
Mike Morel <michael.morel@techelevator.com>
2020-03-26 13:17:44 -0400

    M4D8 lecture

---
+++ module-4/08_Vue_Data_Binding/lecture-final/vue-add-review/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <add-review bookTitle="Veeck - As in Wreck"/>
+  </div>
+</template>
+
+<script>
+import AddReview from './components/AddReview.vue'
+
+export default {
+  name: 'app',
+  components: {
+    AddReview
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ module-4/08_Vue_Data_Binding/lecture-final/vue-chart/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <nutrition-chart class="nutrition"></nutrition-chart>
+  </div>
+</template>
+
+<script>
+import Nutrition  from './components/Nutrition.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'nutrition-chart' : Nutrition
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
