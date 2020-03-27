dc7b642e1b05755610262e427422cd5de2851a42
Mike Morel <michael.morel@techelevator.com>
2020-03-27 12:28:15 -0400

    M4D9 lecture

---
+++ module-4/09_Vue_Event_Handling/lecture-final/vue-chart/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <nutrition-chart class="nutrition"></nutrition-chart>
+  </div>
+</template>
+
+<script>
+import Nutrition  from './components/Nutrition.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'nutrition-chart' : Nutrition
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 100%;
+}
+</style>
