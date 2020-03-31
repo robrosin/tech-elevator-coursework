6b8b4aeaa5b72cea426105fe49df83fe00ef741b
Mike Morel <michael.morel@techelevator.com>
2020-03-31 15:26:39 -0400

    M4D10 lecture

---
+++ module-4/10_Vue_Nested_Components/lecture-final/student-tutorial/todo-lists/src/components/TodoSearch.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="search">
+        <input type="text" id="search" name="search" 
+        v-on:keyup="searchChanged" 
+        v-model="searchString"
+        placeholder="Search Todo's..."/>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            searchString : ''
+        }
+    },
+    methods: {
+        searchChanged() {
+            this.$emit('search-change', this.searchString);
+        }
+    }
+}
+</script>
+
+<style>
+#search {
+    width:100%;
+    font-size:24px;
+    font-family: 'Roboto Condensed', sans-serif;
+    margin-top:50px;
+    padding:10px;
+}
+</style>
---
+++ module-4/10_Vue_Nested_Components/lecture-final/vue-chart/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app">
+    <!-- TODO 03: Handle the data-change event and update the nutritionData data property -->
+    <!-- TODO 05: Add a value for the userName property -->
+    <nutrition v-on:data-change="onDataChange"></nutrition>
+    <div class="charts">
+      <!-- TODO 01: Add a stacked-bar-chart component to display the data graphically. Bind the chart to nutritionData -->
+      <stacked-bar-chart class="chart" v-bind:chartData="nutritionData"></stacked-bar-chart>
+      <!-- TODO 05: Display the chart only if there is any nutritionData -->
+      <!-- TODO 05: Add a title property to the chart -->
+
+
+      <!-- TODO 01: Add a bar-chart component to display the data graphically. Bind the chart to nutritionData -->
+      <bar-chart class="chart" v-bind:chartData="nutritionData" v-bind:scaleToLargest="false" ></bar-chart>
+      <!-- TODO 05: Display the chart only if there is any nutritionData -->
+      <!-- TODO 05: Add a title property to the chart -->
+      <!-- TODO 05: Pass in the scaleToLargest property -->
+
+    </div>
+  </div>
+</template>
+
+<script>
+// TODO 01: Add components to the App (BarChart and StackedBarChart)
+// TODO 03: Handle the data-change event in App.vue to pass the data on to other components
+import Nutrition from "./components/Nutrition.vue";
+import BarChart from "./components/BarChart.vue";
+import StackedBarChart from "./components/StackedBarChart.vue";
+
+export default {
+  name: "app",
+  components: {
+    nutrition: Nutrition,
+    "stacked-bar-chart" : StackedBarChart,
+    "bar-chart" : BarChart
+  },
+  data() {
+    return {
+      /* TODO 03a: Add a nutritionData property to store the array of nutrition data in objects that have: label, value, color properties */
+      nutritionData: []
+    };
+  },
+  methods: {
+    // TODO 03b: Create an onDataChange method to update nutritionData when the table changes
+    /**
+     * Whenever the nutrition data changes in the nutrition component, we change it here.  This property is bound to the charts,
+     * so they will automatically get updated.
+     */
+    onDataChange(newChartData) {
+      this.nutritionData = newChartData;
+    }
+
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 100%;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.charts {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.chart {
+  width: 48%;
+  margin: 10px;
+}
+
+@media screen and (max-width: 1200px) {
+  .chart {
+    width: 100%;
+  }
+}
+</style>
